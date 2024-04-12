d1d84f26bfc2acf092f2926bd7e38b054827f779
Lachlan Williams <lwil0039@student.monash.edu>
2024-04-12 13:57:45 +1000

    Added Job preview

---
+++ IOSAppAssignment/NannyPreviewJobViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NannyPreviewJobViewController.swift
+//  IOSAppAssignment
+//
+//  Created by Lachlan J Williams on 12/4/2024.
+//
+
+import UIKit
+
+class NannyPreviewJobViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
