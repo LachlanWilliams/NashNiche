ba1422bd7bd26840722b13468cc44a60db2f0729
Lachlan Williams <lwil0039@student.monash.edu>
2024-04-17 17:31:15 +1000

    perform signup check

--- IOSAppAssignment/signUpViewController.swift
+++ IOSAppAssignment/signUpViewController.swift
@@ -52,11 +52,29 @@ class signUpViewController: UIViewController {
     
     
     @IBAction func performSignUp(_ sender: Any) {
-        if userType == UserType.nanny {
-            performSegue(withIdentifier: "segueNannyHome", sender: userType)
-        } else {
-            performSegue(withIdentifier: "segueParentHome", sender: userType)
+        guard let firstName = firstNameText.text, !firstName.isEmpty,
+              let lastName = lastNameText.text, !lastName.isEmpty,
+              let email = emailText.text, !email.isEmpty,
+              let password = passwordText.text, !password.isEmpty else {
+            // If any field is empty, display an alert
+            let alert = UIAlertController(title: "Error", message: "Please fill in all fields", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
         }
+        
+        // If all fields are completed, create an alert with the user's information
+        let userInfo = "First Name: \(firstName)\nLast Name: \(lastName)\nEmail: \(email)\nPassword: \(password)"
+        let alert = UIAlertController(title: "User Information", message: userInfo, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            // Perform segue based on user type after dismissing the alert
+            if self.userType == UserType.nanny {
+                self.performSegue(withIdentifier: "segueNannyHome", sender: self.userType)
+            } else {
+                self.performSegue(withIdentifier: "segueParentHome", sender: self.userType)
+            }
+        }))
+        present(alert, animated: true, completion: nil)
     }
     
     /*
